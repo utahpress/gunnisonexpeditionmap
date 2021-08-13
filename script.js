84243d5000a7ad5c1182dd6c5bf1fff3b8069492
utahpress <86483398+utahpress@users.noreply.github.com>
2021-08-13 09:01:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -242,7 +242,7 @@ var initMap = function() {
   L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
-    maxZoom: 7
+    maxZoom: 19
   }).addTo(map);
 
   loadData(dataLocation);
